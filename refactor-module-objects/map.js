591907c00867cc66db679e51915f9e7ef86d9135
Ofekal <Ofekal@users.noreply.github.com>
2025-12-26 11:42:48 +0000

    deploy: c4e8c3cec7b79dc778e590e92d7d03e0a8609f9a

--- refactor-module-objects/map.js
+++ refactor-module-objects/map.js
@@ -237,7 +237,7 @@ const MapManager = {
     teleport(location) {
         const state = GameState;
         state.player.location = location;
-        state.player.position = 630;
+        state.player.position = Constants.PLAYER_POSITION_X;
         clearInterval(state.gameBeat);
         this.enter();
     },
